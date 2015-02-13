8b59e238983458a272a343fd06c5852fa2ccfee2
Philip Orchard <PhilipOrchard@gmail.com>
2015-02-13 11:56:29 -0800

    Add passing Scalene spec

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,9 @@
 var triangle = function( a, b, c ) {
   if (a + b <= c) {
-    return 'This is not a triangle'
+    return "Not a triangle"
   } else if ((a === b) && (b === c)) {
-    return "This is an Equilateral triangle"
+    return "Equilateral"
+  } else if ((a !== b) && (b !== c)) {
+    return "Scalene"
   };
 };
--- spec/specs.js
+++ spec/specs.js
@@ -1,9 +1,13 @@
 describe('triangle', function() {
   it("will return false if one side is at least as long as the other two combined", function() {
-    expect(triangle(2,2,8)).to.equal('This is not a triangle');
+    expect(triangle(2,2,8)).to.equal('Not a triangle');
   });
 
-  it("will return Equilateral", function(){
-    expect(triangle(6,6,6)).to.equal("This is an Equilateral triangle");
+  it("will return Equilateral triangle if all sides equal", function(){
+    expect(triangle(6,6,6)).to.equal("Equilateral");
   });
+
+  it("will return Scalene if no sides equal", function (){
+    expect(triangle(4,5,6)).to.equal("Scalene");
+  })
 });
