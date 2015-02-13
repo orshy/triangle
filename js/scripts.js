7f58e8c970b1e5fda92be5588a06d0406d10f0d3
Philip Orchard <PhilipOrchard@gmail.com>
2015-02-13 11:32:09 -0800

    Add passing equlateral spec

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,7 @@
 var triangle = function( a, b, c ) {
   if (a + b <= c) {
     return 'This is not a triangle'
-  }
-}
+  } else if ((a === b) && (b === c)) {
+    return "This is an Equilateral triangle"
+  };
+};
--- spec/specs.js
+++ spec/specs.js
@@ -2,4 +2,8 @@ describe('triangle', function() {
   it("will return false if one side is at least as long as the other two combined", function() {
     expect(triangle(2,2,8)).to.equal('This is not a triangle');
   });
+
+  it("will return Equilateral", function(){
+    expect(triangle(6,6,6)).to.equal("This is an Equilateral triangle");
+  });
 });
